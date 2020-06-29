5c3ff0a5eff9cc43eceec4570673cc1a3b7e0906
Austin Bean <austinbean@gmail.com>
2020-06-29 10:36:28 -0400

    initial

---
+++ word_counting.jl
@@ -0,0 +1,23 @@
+# word counting exercise
+
+
+using CSV
+using DataFrames
+
+data = CSV.file("very_fake_diet_data.csv") |> DataFrame
+
+function Counter(d::DataFrame)
+	outp = Dict{String, Int64}()
+	for i = 1:size(data,1)
+		for j in split(data[i,1])
+			if haskey(outp, j)
+				outp[j] += 1
+			else 
+				outp[j] = 1 
+			end 
+		end 
+	end 
+	return outp
+end 
+
+Counter(data)
